3c7fc6d4c55ef55144c3b74c775570d3fed6a4d0
sjw <https://github.com/FighttingEDG/ag-end.git>
2025-03-05 11:28:38 +0800

    数据显示

--- src/App.jsx
+++ src/App.jsx
@@ -9,17 +9,24 @@ function App() {
   return (
     <div className='container'>
       this is app
+      {/* 引号传递 */}
       <div>
         {message}
       </div>
       <div>
         {'字符串传递'}
       </div>
+      {/* 函数和方法调用 */}
       <div>
         {getName()}
       </div>
+      {/* js对象 */}
       <div>
         {new Date().getDate()}
+      </div>
+      {/* 识别表达式的语法 */}
+      <div style={{ color: 'red' }}>
+        带颜色文字
       </div>
     </div>
   )
