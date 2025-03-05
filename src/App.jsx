36b7695860c257db2fc4e44ebd453501d8f5e654
sjw <https://github.com/FighttingEDG/ag-end.git>
2025-03-05 11:24:16 +0800

    数据的引用

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,27 @@
 import { useState } from 'react'
-
+const message = "消息"
+function getName() {
+  return 'xiaobai';
+}
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <>
-    this is app
-    </>
+    <div className='container'>
+      this is app
+      <div>
+        {message}
+      </div>
+      <div>
+        {'字符串传递'}
+      </div>
+      <div>
+        {getName()}
+      </div>
+      <div>
+        {new Date().getDate()}
+      </div>
+    </div>
   )
 }
 
