28e6f9b2dc29741399001424884a4a8d4596af32
sjw <https://github.com/FighttingEDG/ag-end.git>
2025-03-05 14:35:04 +0800

    组件

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,16 @@
 import { useState } from 'react'
-// 绑定事件
-function handlerClick() {
-  console.log('很快')
-}
-function handlerClick2(str) {
-  console.log(str)
-}
-function handlerClick3(str, e) {
-  console.log(str)
-  console.log(e)
+// 组件，一定要首字母大写
+function Component1(){
+  return <button onClick={()=>{console.log('哈哈')}}>组件按钮</button>
 }
 function App() {
   const [count, setCount] = useState(0)
   return (
     <div>
-      {/* 不传参 */}
-      <button onClick={handlerClick}>按钮</button>
-      {/* 传参 */}
-      <button onClick={() => handlerClick2('哈哈')}>按钮</button>
-      {/* 事件对象 */}
-      <button onClick={(e) => handlerClick3('哈哈', e)}>按钮</button>
+      {/* 自闭和 */}
+      <Component1 />
+      {/* 成对标签 */}
+      <Component1></Component1>
     </div>
   )
 }
