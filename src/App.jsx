b612bb87fe43a160d259decaa3225fc94295e9e7
sjw <https://github.com/FighttingEDG/ag-end.git>
2025-03-05 13:45:51 +0800

    列表渲染

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,26 @@
 import { useState } from 'react'
-const message = "消息"
-function getName() {
-  return 'xiaobai';
-}
+const list = [
+  {
+    id: 1001,
+    name: 'vue'
+  },
+  {
+    id: 1002,
+    name: 'react'
+  },
+  {
+    id: 1003,
+    name: 'angular'
+  }
+]
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <div className='container'>
-      this is app
-      {/* 引号传递 */}
-      <div>
-        {message}
-      </div>
-      <div>
-        {'字符串传递'}
-      </div>
-      {/* 函数和方法调用 */}
-      <div>
-        {getName()}
-      </div>
-      {/* js对象 */}
-      <div>
-        {new Date().getDate()}
-      </div>
-      {/* 识别表达式的语法 */}
-      <div style={{ color: 'red' }}>
-        带颜色文字
-      </div>
+      <ul>
+        {list.map(item => <li key={item.id}>{item.name}</li>)}
+      </ul>
     </div>
   )
 }
