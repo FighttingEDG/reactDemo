9c672119df6ee36d7e936a252bec4c49fcfe6486
sjw <https://github.com/FighttingEDG/ag-end.git>
2025-03-05 14:11:58 +0800

    点击事件传参与不传参

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,19 @@
 import { useState } from 'react'
-// 三种情况渲染
-const list = [0, 1, 3]
-function getContent(num) {
-  if (num == 0) {
-    return <div>这是数字0的样式</div>
-  } else if (num == 1) {
-    return <div>这是数字1的样式</div>
-  } else {
-    return <div>这是数字3的样式</div>
-  }
+// 绑定事件
+function handlerClick() {
+  console.log('很快')
+}
+function handlerClick2(str) {
+  console.log(str)
 }
 function App() {
   const [count, setCount] = useState(0)
-
   return (
-    <div className='container'>
-      <ul>
-        {list.map(item => <li key={item}>{getContent(item)}</li>)}
-      </ul>
+    <div>
+    {/* 不传参 */}
+      <button onClick={handlerClick}>按钮</button>
+    {/* 传参 */}
+      <button onClick={()=>handlerClick2('哈哈')}>按钮</button>
     </div>
   )
 }
