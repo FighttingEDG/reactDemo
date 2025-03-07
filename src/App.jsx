75663488133c692815d986f40db4df6833657018
sjw <https://github.com/FighttingEDG/ag-end.git>
2025-03-07 14:12:53 +0800

    跨层通信

--- src/App.jsx
+++ src/App.jsx
@@ -1,36 +1,36 @@
-import { use, useState, useRef } from 'react'
+import { useState, createContext, useContext } from 'react'
 import './App.scss'
 import avatar from './images/bozai.png'
 import _ from 'lodash'
 import { v4 as uuidv4 } from 'uuid';
 import dayjs from 'dayjs';
-// 兄弟组件通信
-function A({ sendStr }) {
-  const AName = 'a兄弟数据'
+// app传数据给B
+// App组件套A，A套B组件
+const mesCtx = createContext();
+function A() {
   return <div>
-    A兄弟组件
-    <button onClick={() => sendStr(AName)}>A按钮</button>
+    这是A组件
+    {/* B组件 */}
+    <B />
   </div>
 }
-function B({AStr}) {
+function B() {
+  // 使用对应的上下文
+  const ctxMes = useContext(mesCtx)
   return <div>
-    B兄弟组件
+    这是B组件
     <br />
-    {/* A组件字符串 */}
-    {AStr}
-    </div>
+    {ctxMes}
+  </div>
 }
 const App = () => {
-  const [AStr, setAStr] = useState(null)
-  const sendStr = (str) => {
-    setAStr(str)
-  }
+  const mes = '顶层组件数据'
   return (
     <div className="app">
-      <A sendStr={sendStr} />
-      <B AStr={AStr}/>
+      <mesCtx.Provider value={mes}>
+        <A />
+      </mesCtx.Provider>
     </div>
   )
 }
-
 export default App
