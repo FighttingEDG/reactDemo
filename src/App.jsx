c595bc8e8dff9d2e2417722eb1942039bb18a706
sjw <https://github.com/FighttingEDG/ag-end.git>
2025-03-07 10:40:21 +0800

    父传子传递jsx，其他数组，对象也可以传递

--- src/App.jsx
+++ src/App.jsx
@@ -10,13 +10,15 @@ function Son(props){
   return <div>
     这是子组件
     <div>{props.appName}</div>
+    {props.jsx}
   </div>
 }
 const App = () => {
   const appName = '父组件数据'
   return (
     <div className="app">
-      <Son appName={appName}/>
+      {/* 可传递最复杂的jsx */}
+      <Son appName={appName} jsx={<span>这是个span标签</span>}/>
     </div>
   )
 }
