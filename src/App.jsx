35a3fd95d66fc3d9c79a6d407e3ac0f75753f10d
sjw <https://github.com/FighttingEDG/ag-end.git>
2025-03-07 15:37:40 +0800

    有依赖性情况，类似于vue的监听

--- src/App.jsx
+++ src/App.jsx
@@ -3,14 +3,14 @@ import { useState, useEffect } from 'react'
 const App = () => {
   // 1.没有依赖项   初始+状态更新
   // 2.空数组       初始
-  useEffect(() => {
-    console.log('空数组依赖项执行了')
-  },[])
+  // 3.有依赖项      初始+依赖项更新+状态更新（后两者算一次）
   const [count, setCount] = useState(0)
-  // 3.有依赖项      初始+依赖项更新+状态更新
+  useEffect(() => {
+    console.log('有依赖项依赖项执行了')
+  },[count])
   return (
     <div className="app">
-      <div>空数组依赖项{count}</div>
+      <div>有依赖项依赖项{count}</div>
       <button onClick={() => setCount(count + 1)}>+1</button>
     </div>
   )
