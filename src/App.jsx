f5f9a8fca8664aaf9a24c5546ad4f8156712319d
sjw <https://github.com/FighttingEDG/ag-end.git>
2025-03-07 15:31:43 +0800

    空数组依赖项执行了

--- src/App.jsx
+++ src/App.jsx
@@ -2,16 +2,15 @@ import { useState, useEffect } from 'react'
 // 不同依赖项
 const App = () => {
   // 1.没有依赖项   初始+状态更新
-  useEffect(() => {
-    console.log('没有依依赖项执行了')
-  })
-  const [count, setCount] = useState(0)
   // 2.空数组       初始
+  useEffect(() => {
+    console.log('空数组依赖项执行了')
+  },[])
+  const [count, setCount] = useState(0)
   // 3.有依赖项      初始+依赖项更新+状态更新
   return (
     <div className="app">
-      <div>没有依赖项{count}</div>
-      {/* 状态更新就会触发 */}
+      <div>空数组依赖项{count}</div>
       <button onClick={() => setCount(count + 1)}>+1</button>
     </div>
   )
