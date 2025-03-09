fdcb6897708408234fa7fb5693df0615e7a25629
sjw <https://github.com/FighttingEDG/ag-end.git>
2025-03-09 22:14:56 +0800

    封装item组件

--- src/App.jsx
+++ src/App.jsx
@@ -3,46 +3,6 @@ import './App.scss'
 import avatar from './images/bozai.png'
 import React, { useState, useEffect } from 'react'
 
-// 评论列表数据
-const defaultList = [
-  {
-    // 评论id
-    rpid: 3,
-    // 用户信息
-    user: {
-      uid: '13258165',
-      avatar: '',
-      uname: '周杰伦',
-    },
-    // 评论内容
-    content: '哎哟，不错哦',
-    // 评论时间
-    ctime: '10-18 08:15',
-    like: 88,
-  },
-  {
-    rpid: 2,
-    user: {
-      uid: '36080105',
-      avatar: '',
-      uname: '许嵩',
-    },
-    content: '我寻你千百度 日出到迟暮',
-    ctime: '11-13 11:29',
-    like: 88,
-  },
-  {
-    rpid: 1,
-    user: {
-      uid: '30009257',
-      avatar,
-      uname: '黑马前端',
-    },
-    content: '学前端就来黑马',
-    ctime: '10-19 09:00',
-    like: 66,
-  },
-]
 // 当前登录用户信息
 const user = {
   // 用户id
@@ -53,20 +13,6 @@ const user = {
   uname: '黑马前端',
 }
 
-/**
- * 导航 Tab 的渲染和操作
- *
- * 1. 渲染导航 Tab 和高亮
- * 2. 评论列表排序
- *  最热 => 喜欢数量降序
- *  最新 => 创建时间降序
- */
-
-// 导航 Tab 数组
-const tabs = [
-  { type: 'hot', text: '最热' },
-  { type: 'time', text: '最新' },
-]
 
 // 封装获取数据的hook
 function useGetList() {
@@ -83,10 +29,61 @@ function useGetList() {
     setCommentList
   }
 }
+/**
+ * 导航 Tab 的渲染和操作
+ *
+ * 1. 渲染导航 Tab 和高亮
+ * 2. 评论列表排序
+ *  最热 => 喜欢数量降序
+ *  最新 => 创建时间降序
+ */
+
+// 导航 Tab 数组
+const tabs = [
+  { type: 'hot', text: '最热' },
+  { type: 'time', text: '最新' },
+]
+function Item({ item }) {
+  return (<div className="reply-item" key={item.rpid}>
+    {/* 头像 */}
+    <div className="root-reply-avatar">
+      <div className="bili-avatar">
+        <img
+          className="bili-avatar-img"
+          alt=""
+          src={avatar}
+        />
+      </div>
+    </div>
+
+    <div className="content-wrap">
+      {/* 用户名 */}
+      <div className="user-info">
+        <div className="user-name">{item.user.uname}</div>
+      </div>
+      {/* 评论内容 */}
+      <div className="root-reply">
+        <span className="reply-content">{item.content}</span>
+        <div className="reply-info">
+          {/* 评论时间 */}
+          <span className="reply-time">{item.ctime}</span>
+          {/* 评论数量 */}
+          <span className="reply-time">点赞数:{item.like}</span>
+          {/* user下的id等于评论list下的uid才显示删除 */}
+          {user.uid === item.user.uid && <span className="delete-btn" onClick={() => handlerDel(item.rpid)}>删除</span>}
+        </div>
+      </div>
+    </div>
+  </div>)
+}
 const App = () => {
-  // 结构后继续使用
   const { commentList, setCommentList } = useGetList();
 
+  // 因为处理的是useState，所以需要在组件内部处理数组
+  const handlerDel = (id) => {
+    // 过滤defaultList
+    setDefaultList(defaultList.filter(item => item.rpid !== id))
+  }
   return (
     <div className="app">
       {/* 导航 Tab */}
@@ -129,38 +126,7 @@ const App = () => {
         {/* 评论列表 */}
         <div className="reply-list">
           {/* 评论项 */}
-          <div className="reply-item">
-            {/* 头像 */}
-            <div className="root-reply-avatar">
-              <div className="bili-avatar">
-                <img
-                  className="bili-avatar-img"
-                  alt=""
-                />
-              </div>
-            </div>
-
-            <div className="content-wrap">
-              {/* 用户名 */}
-              <div className="user-info">
-                <div className="user-name">jack</div>
-              </div>
-              {/* 评论内容 */}
-              <div className="root-reply">
-                <span className="reply-content">这是一条评论回复</span>
-                <div className="reply-info">
-                  {/* 评论时间 */}
-                  <span className="reply-time">{'2023-11-11'}</span>
-                  {/* 评论数量 */}
-                  <span className="reply-time">点赞数:{100}</span>
-                  <span className="delete-btn">
-                    删除
-                  </span>
-
-                </div>
-              </div>
-            </div>
-          </div>
+          {commentList.map(item => <Item item={item} key={item.rpid} />)}
         </div>
       </div>
     </div>
