8ebc9cf87494cf0531a36904a4faac0c1a709a32
sjw <https://github.com/FighttingEDG/ag-end.git>
2025-03-17 21:21:14 +0800

    类组件父传子

--- src/App.jsx
+++ src/App.jsx
@@ -1,37 +1,27 @@
 import React, { Component, useState } from 'react'
 
-class Counter extends Component {
-  // 定义状态变量
-  state = { count: 0 }
-  componentDidMount() {
-    console.log('组件挂载完成')
-    this.timer = setInterval(() => {
-      console.log('定时器')
-    }, 1000)
-  }
-  componentWillUnmount() {
-    console.log('组件即将卸载')
-    // 清除定时器
-    clearInterval(this.timer)
-  }
-  // 事件回调
-  clickHandler = () => {
-    this.setState({
-      count: this.state.count + 1
-    })
-  }
-  // ui模板(jsx)
+class Son extends Component {
   render() {
-    return <button onClick={this.clickHandler}>{this.state.count}</button>
+    return <div>子组件<br />{this.props.msg}</div>
+  }
+}
+class Parent extends Component {
+  state = {
+    msg: '父的数据'
+  }
+  render() {
+    return <div>
+      父组件，下面是子组件
+      <br />
+      <Son msg={this.state.msg} />
+    </div>
   }
 }
 
 const App = () => {
-  const [show, setShow] = useState(true)
   return (
     <div className="app">
-      {show && <Counter />}
-      <button onClick={() => setShow(false)}>卸载</button>
+      <Parent />
     </div>
   )
 }
