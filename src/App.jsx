76f96bec499ee81eba7c666b89af8bc79790f9cb
sjw <https://github.com/FighttingEDG/ag-end.git>
2025-03-17 21:25:33 +0800

    子传父

--- src/App.jsx
+++ src/App.jsx
@@ -2,28 +2,24 @@ import React, { Component, useState } from 'react'
 
 class Son extends Component {
   render() {
-    return <div>子组件<br />{this.props.msg}</div>
+    return <div>子组件
+      <button onClick={() => this.props.sendFun('我是子组件中的数据')}>发送数据</button>
+    </div>
   }
 }
 class Parent extends Component {
-  state = {
-    msg: '父的数据'
+  sendFun = (sonMsg) => {
+    console.log(sonMsg)
   }
   render() {
     return <div>
-      父组件，下面是子组件
-      <br />
-      <Son msg={this.state.msg} />
+      父组件
+      <Son sendFun={this.sendFun} />
     </div>
   }
 }
 
 const App = () => {
-  return (
-    <div className="app">
-      <Parent />
-    </div>
-  )
 }
 
 export default App
